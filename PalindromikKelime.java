f801de5258bfa3e254835802008d18204e3199ff
Rukiye AR <130596473+rukiyear@users.noreply.github.com>
2026-01-03 12:55:53 +0300

    Add files via upload

---
+++ PalindromikKelime.java
@@ -0,0 +1,27 @@
+public class PalindromikKelime {
+
+    static boolean isPalindrome(String str){
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str){
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return (str.equals(reverse));
+    }
+
+    static void main() {
+        System.out.println(isPalindrome2("kayak"));
+    }
+}
